0590123fb1cfc77440cc90a30f33a3304ff4610d
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2024-12-27 21:17:39 +0100

    delete js file in src

--- src/preload.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld('api', {
-    loadWords: () => electron_1.ipcRenderer.invoke('load-words'),
-    censorText: (inputText, words) => electron_1.ipcRenderer.invoke("censor-text", { inputText, words }),
-});
